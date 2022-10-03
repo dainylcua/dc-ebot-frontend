9a086c2d5ef83c4de3b390f81434686f5220333a
Dainyl Cua <83785249+dainylcua@users.noreply.github.com>
2022-10-03 10:56:18 -0500

    Adds notes page and initial data load succesful

---
+++ pages/notes.tsx
@@ -0,0 +1,10 @@
+import Container from "../components/Container"
+
+export default function Notes() {
+  // TODO: Add notes about challenges faced during development
+  return (
+    <Container>
+      I encountered a few challenges:
+    </Container>
+  )
+}
--- pages/stations/index.tsx
+++ pages/stations/index.tsx
@@ -1,7 +1,24 @@
 import Container from "../../components/Container"
+import { useEffect, useState, useMemo } from "react";
+
+const getData = async () => {
+  const res = await fetch(`http://localhost:3001/stations`)
+  const data = await res.json();
+  return data
+}
 
 export default function Home() {
   // TODO: Add information and improve layout
+
+  const [stations, setStations] = useState([]);
+
+  useEffect(() => {
+      getData().then((stations) => {
+        console.log(stations)
+        setStations(stations)
+      })
+  }, [])
+  
   return (
     <Container>
       Brief intro about the plant, location, information, etc.
